3c970c02e0794fbef9ff7f5e558556f1a39a058d
Marconi Soares <marconi.developer@outlook.com.br>
2023-02-07 18:05:39 -0300

    aula-14

---
+++ tests/aula-14/choose_name.py
@@ -0,0 +1,7 @@
+from random import choice 
+
+names = ['Marconi', 'Maria', 'Marcela', 'Miguel']
+
+name = choice(names)
+print(name)
+
---
+++ tests/aula-14/say_hi.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------
+# Script     :
+# Descrição  :
+# Versão     : 1.0
+# Autor      : Marconi S. de Jesus <marconi.developer@outlook.com.br>
+# Licença    : GNU/GPL v3.0
+# Data       : 07/02/2023
+# ------------------------------------------------------------
+# Uso:
+# ------------------------------------------------------------
+
+nome=$( python3 choose_name.py )
+
+echo "Hi, $nome"
