42ad0b8cf4b8841a02e742638befbf55c338d5ff
Marconi Soares <marconi.developer@outlook.com.br>
2023-02-11 23:06:14 -0300

    aula-18

---
+++ tests/aula-18/teste-mod.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------
+# Script     :
+# Descrição  :
+# Versão     : 1.0
+# Autor      : Marconi S. de Jesus <marconi.developer@outlook.com.br>
+# Licença    : GNU/GPL v3.0
+# Data       : 11/02/2023
+# ------------------------------------------------------------
+# Uso:
+# ------------------------------------------------------------
+teste_oi() {
+  echo "Oi, eu sou $1"
+}
